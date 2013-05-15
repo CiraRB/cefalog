55caf5c466353987a59d1a91c2c6ea063a3f310b
CiraRB <conxura@gmail.com>
2013-05-15 11:01:23 +0200

    DatePicker and TimePicker with fragments

--- src/com/cirarb/cefalog/NewEntryActivity.java
+++ src/com/cirarb/cefalog/NewEntryActivity.java
@@ -1,23 +1,34 @@
 package com.cirarb.cefalog;
 
+import java.text.ParseException;
+import java.util.Calendar;
+
+import com.cirarb.cefalog.fragments.DatePickerFragment;
+import com.cirarb.cefalog.fragments.TimePickerFragment;
+
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
+import android.text.format.DateFormat;
 import android.annotation.TargetApi;
 import android.os.Build;
 
-public class NewEntryActivity extends Activity {
+public class NewEntryActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_entry);
-		// Show the Up button in the action bar.
-		setupActionBar();
-	}
 
+		setupActionBar();
+		setupDateTimeButtons();
+	}
+	
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
 	 */
@@ -39,17 +50,60 @@ public class NewEntryActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void setupDateTimeButtons(){
+		java.text.DateFormat dateFormat = DateFormat.getDateFormat(getApplicationContext());
+		java.text.DateFormat timeFormat = DateFormat.getTimeFormat(getApplicationContext());
+		
+		Calendar c = Calendar.getInstance();
+		
+		Button btnDate = (Button) findViewById(R.id.btnDate);
+		btnDate.setText(dateFormat.format(c.getTime()));
+		btnDate.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	try {
+					showDatePickerDialog(R.id.btnDate);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        });
+		
+		Button btnTime = (Button) findViewById(R.id.btnTime);
+		btnTime.setText(timeFormat.format(c.getTime()));
+		btnTime.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	try {
+            		showTimePickerDialog(R.id.btnTime);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+        });
+	}
 
+	public void showDatePickerDialog(int button) throws ParseException {
+		Bundle args = new Bundle();
+        args.putInt("button", button);
+        
+        DatePickerFragment dpf = new DatePickerFragment();
+        dpf.setArguments(args);
+		dpf.show(getSupportFragmentManager(), "datePicker");
+	}
+	
+	public void showTimePickerDialog(int button) throws ParseException {
+		Bundle args = new Bundle();
+        args.putInt("button", button);
+        
+        TimePickerFragment dpf = new TimePickerFragment();
+        dpf.setArguments(args);
+		dpf.show(getSupportFragmentManager(), "timePicker");
+	}
 }
